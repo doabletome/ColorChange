921fe52c81a113a61f1472c8c2d5f531a78f6d95
Himanshu Patle <136967535+doabletome@users.noreply.github.com>
2024-07-02 20:52:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeColor() {
+  const newColor = getRandomColor();
+  document.body.style.backgroundColor = newColor;
+  document.getElementById(
+    "color-code"
+  ).textContent = `Hex Color Code: ${newColor}`;
+}
